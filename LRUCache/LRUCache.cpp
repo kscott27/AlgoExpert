d187b3eea7ff2bd0edc0c27c1aaddc0003de8b10
Kevin Scott <kmscott27@gmail.com>
2020-08-25 10:41:15 -0700

    LRUCache
    - Consolidated

--- LRUCache/LRUCache.cpp
+++ LRUCache/LRUCache.cpp
@@ -10,80 +10,32 @@ public:
   LRUCache(int maxSize) { this->maxSize = maxSize > 1 ? maxSize : 1; }
 
   void insertKeyValuePair(string key, int value) {
-    Node* nodeToInsert = new Node(key, value, NULL, NULL);
-    // Grab current head node
-    auto head = cache_.find(headKey_);
-    if( head != cache_.end() ) {
-      // Place the new node at the head by setting
-      // it as the old head's prev ptr.
-      head->second->prev = nodeToInsert;
-      nodeToInsert->next = head->second;
-    }
     // Check if the key is already present.
     auto oldIt = cache_.find(key);
     if( oldIt != cache_.end() ) {
-      // If the key is already present, update the links of
-      // its surrounding nodes and then remove it from the map.
-      if( oldIt->second->prev != NULL )
-        oldIt->second->prev->next = oldIt->second->next;
-      if( oldIt->second->next != NULL )
-        oldIt->second->next->prev = oldIt->second->prev;
+      updateValue(oldIt->second, value);
+      pushToHead(oldIt->second);
+    }
+    else {
+      Node* nodeToInsert = new Node(key, value, NULL, NULL);
+      cache_.insert({key, nodeToInsert});
+      pushToHead(nodeToInsert);
+    }
 
-      if( tailKey_ == key ) {
-        // Update the tail key if we are moving the old tail
-        // to the head.
-        if( oldIt->second->prev != NULL )
-          tailKey_ = oldIt->second->prev->key;
-      }
-      cache_.erase(oldIt);
-    }
     // Pop the tail if the cache is at max capacity.
-    if( cache_.size() >= this->maxSize ) {
+    if( cache_.size() > this->maxSize ) {
       auto tail = cache_.find(tailKey_);
-      if( tail != cache_.end() ) {
-        if( tail->second->prev != NULL ) {
-          tail->second->prev->next = NULL;
-          tailKey_ = tail->second->prev->key;
-        }
-        cache_.erase(tail);
-      }
+      if( tail != cache_.end() )
+        removeNode(tail);
     }
-    if( cache_.size() == 0 ) {
-      tailKey_ = key;
-    }
-    // Insert the new key-value
-    cache_.insert({key, nodeToInsert});
-    // Update the head key
-    headKey_ = key;
   }
 
   int *getValueFromKey(string key) {
     auto it = cache_.find(key);
     if( it == cache_.end() )
       return NULL;
-    
-    if( tailKey_ == key ) {
-      // Update the tail key if we are moving the old tail
-      // to the head.
-      if( it->second->prev != NULL )
-        tailKey_ = it->second->prev->key;
-    }
-    
-    if( it->second->prev != NULL )
-      it->second->prev->next = it->second->next;
-    if( it->second->next != NULL )
-      it->second->next->prev = it->second->prev;
-    
-    it->second->prev = NULL;
-    // Grab current head node
-    auto head = cache_.find(headKey_);
-    if( head != cache_.end() ) {
-      // Place the new node at the head by setting
-      // it as the old head's prev ptr.
-      head->second->prev = it->second;
-      it->second->next = head->second;
-    }
-    headKey_ = key;
+
+    pushToHead(it->second);
     
     return &(it->second->value);
   }
@@ -106,6 +58,52 @@ private:
     Node* prev;
     Node* next;
   };
+  
+  void pushToHead(Node* node) {
+    // If the node had already existed, remove its
+    // links before updating. Does nothing if it
+    // was not in the list to begin with.
+    removeNodeLinks(node);
+    
+    // Grab current head node
+    auto head = cache_.find(headKey_);
+    if( head != cache_.end() ) {
+      // Place the new node at the head by setting
+      // it as the old head's prev ptr.
+      head->second->prev = node;
+      node->next = head->second;
+      node->prev = NULL;
+    }
+    
+    if( cache_.size() == 1 )
+      tailKey_ = node->key;
+    
+    headKey_ = node->key;
+  }
+    
+  void updateValue(Node* node, int value) {
+    node->value = value;
+  }
+  
+  void removeNodeLinks(Node* node) {
+    if( node->prev != NULL )
+      node->prev->next = node->next;
+    if( node->next != NULL )
+      node->next->prev = node->prev;
+    
+    if( tailKey_ == node->key ) {
+      // Update the tail key if we are moving the old tail
+      // to the head.
+      if( node->prev != NULL )
+        tailKey_ = node->prev->key;
+    }
+  }
+    
+  void removeNode(unordered_map<string, Node*>::iterator& it) {
+    removeNodeLinks(it->second);
+    cache_.erase(it);
+  }
+  
   unordered_map<string, Node*> cache_;
   string headKey_;
   string tailKey_;
