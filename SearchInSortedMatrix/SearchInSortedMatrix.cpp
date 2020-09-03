122e95ccb18eb9a9fa3a48105b269af9d8edd969
Kevin Scott <kmscott27@gmail.com>
2020-09-02 18:12:21 -0700

    SearchInSortedMatrix

---
+++ SearchInSortedMatrix/SearchInSortedMatrix.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+vector<int> searchInSortedMatrix(vector<vector<int>> matrix, int target) {
+  int row = 0;
+  int col = matrix[0].size() - 1;
+  while( row < matrix.size() && col >= 0 ) {
+    if( matrix[row][col] > target ) {
+      col--;
+    }
+    else if( matrix[row][col] < target ) {
+      row++;
+    }
+    else {
+      return {row, col};
+    }
+  }
+  return {-1, -1};
+}
